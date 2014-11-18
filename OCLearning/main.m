d59b43a5028941ab753fb821260d1a6d5062a062
yimin <wym.missyou@gmail.com>
2014-11-18 20:52:02 +0800

    Initial Commit

---
+++ OCLearning/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCLearning
+//
+//  Created by 吴一敏 on 14/11/18.
+//  Copyright (c) 2014年 OCLearning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
