177af708ecf7e3dabbdcdfd88869dc9cf61ffd32
WuYimin <wym.missyou@gmail.com>
2015-04-16 15:05:28 +0800

    递归的方法计算斐波那契数

---
+++ OCLearning/Chapter13/Fibonacci.h
@@ -0,0 +1,15 @@
+//
+// Created by 吴一敏 on 15/4/16.
+// Copyright (c) 2015 OCLearning. All rights reserved.
+//
+
+#import "Learning.h"
+
+@interface Fibonacci : Learning
+
+@property int fibonacci;
+
++ (Fibonacci *)on:(int)fib;
+
++ (void)test:(int)fibonacci;
+@end
